3d8638fccd9cf48d8ce76487303bc0b4791b3b10
victor de souza magaldi <victmagaldi@hotmail.com>
2022-10-13 17:59:16 -0300

    app file next js

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
@@ -16,5 +16,3 @@ const GlobalStyles = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
-
-export default GlobalStyles
